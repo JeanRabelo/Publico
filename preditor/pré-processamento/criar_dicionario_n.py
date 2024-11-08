dbdb97bada85015d3312f18982958eb122ed32d8
JeanRabelo <jeanpablosousarabelo@gmail.com>
2024-11-08 00:49:31 -0300

    adicionando a constituição americana e o script do frozen

--- preditor/pré-processamento/criar_dicionario_n.py
+++ preditor/pré-processamento/criar_dicionario_n.py
@@ -2,9 +2,9 @@ import json
 import re
 from collections import defaultdict, Counter
 
-def build_word_followers(text, n=2):
-    # Split the text into words
-    words = re.findall(r'\b\w+\b', text.lower())
+def build_word_followers(text, n=2, max_predictions=3):
+    # Split the text into words, allowing apostrophes within words
+    words = re.findall(r"\b[\w']+\b", text.lower())
     word_followers = defaultdict(list)
     
     # Build a dictionary with each n-word combination and its followers
@@ -13,24 +13,25 @@ def build_word_followers(text, n=2):
         next_word = words[i + n]
         word_followers[current_words].append(next_word)
     
-    # For each n-word combination, get the three most common followers
-    word_followers_top3 = {phrase: [f[0] for f in Counter(followers).most_common(3)]
-                           for phrase, followers in word_followers.items()}
+    # For each n-word combination, get the most common followers up to max_predictions
+    word_followers_top = {phrase: [f[0] for f in Counter(followers).most_common(max_predictions)]
+                          for phrase, followers in word_followers.items()}
     
-    return word_followers_top3
+    return word_followers_top
 
-def txt_to_json(input_file, output_file, n=2):
+def txt_to_json(input_file, output_file, n=2, max_predictions=3):
     with open(input_file, 'r', encoding='utf-8') as file:
         text = file.read()
     
-    word_followers = build_word_followers(text, n)
+    word_followers = build_word_followers(text, n, max_predictions)
     
     with open(output_file, 'w', encoding='utf-8') as json_file:
         json.dump(word_followers, json_file, ensure_ascii=False, indent=4)
 
 # Usage example
-input_file = 'processed_biblia.txt'  # Replace with your input file path
-output_file = 'output.json'  # Replace with your output file path
-n = 2  # Set the desired number of words in the key
-txt_to_json(input_file, output_file, n)
+input_file = 'constituição_americana.txt'  # Replace with your input file path
+n = 3  # Set the desired number of words in the key
+max_predictions = 5  # Set the maximum number of predictions
+output_file = f'output_{n}_{max_predictions}.json'  # Replace with your output file path
+txt_to_json(input_file, output_file, n, max_predictions)
 
